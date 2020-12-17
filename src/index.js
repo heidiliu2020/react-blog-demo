7c818fc9dd8258e2ec5750bd46da4adc52ebcba1
heidiliu2020 <donkin1110@gmail.com>
2020-12-17 21:47:57 +0800

    update blog

--- src/index.js
+++ src/index.js
@@ -1,26 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { ThemeProvider } from "styled-components";
+import App from "./components/App";
 
-const theme = {
-  colors: {
-    primary_300: "#ff7777",
-    primary_400: "#e33e3e",
-    primary_500: "#af0505",
-  },
-};
-
-ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <App />
-  </ThemeProvider>,
-  document.getElementById("root")
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById("root"));
